a8fbf38d89bf6d1c988c09b7f790bab478a82873
Magnus Erler <magnus.erler@gmail.com>
2022-08-19 20:48:19 +0000

    Added catkin

---
+++ catkin_ws/build/mb_1r2t_ros/CMakeFiles/mb_1r2t_publisher.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/catkin_ws/src/mb_1r2t_ros/src/mb_1r2t.cpp" "/home/ubuntu/catkin_ws/build/mb_1r2t_ros/CMakeFiles/mb_1r2t_publisher.dir/src/mb_1r2t.cpp.o"
+  "/home/ubuntu/catkin_ws/src/mb_1r2t_ros/src/mb_1r2t_publisher_node.cpp" "/home/ubuntu/catkin_ws/build/mb_1r2t_ros/CMakeFiles/mb_1r2t_publisher.dir/src/mb_1r2t_publisher_node.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"mb_1r2t_ros\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ubuntu/catkin_ws/src/mb_1r2t_ros/include"
+  "/opt/ros/noetic/include"
+  "/opt/ros/noetic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ catkin_ws/build/mb_1r2t_ros/CMakeFiles/mb_1r2t_publisher.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/ubuntu/catkin_ws/devel/lib/mb_1r2t_ros/mb_1r2t_publisher"
+  "/home/ubuntu/catkin_ws/devel/lib/mb_1r2t_ros/mb_1r2t_publisher.pdb"
+  "CMakeFiles/mb_1r2t_publisher.dir/src/mb_1r2t.cpp.o"
+  "CMakeFiles/mb_1r2t_publisher.dir/src/mb_1r2t_publisher_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mb_1r2t_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
