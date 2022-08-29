7930796f64253c3392793b4b1d59b96a4e187c79
Magnus Erler <magnus.erler@gmail.com>
2022-08-29 08:16:27 +0000

    Commit after catkin_make

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -8,4 +8,3 @@
 
 # modified environment variables
 export PWD='/home/ubuntu/Robocup2023/catkin_ws/build'
-export PYTHONPATH="/home/ubuntu/Robocup2023/catkin_ws/devel/lib/python3/dist-packages:$PYTHONPATH"
