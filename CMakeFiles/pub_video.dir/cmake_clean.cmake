b8f6b554737c92f328806513ef254ea2a9e9b6da
Magnus Erler <magnus.erler@gmail.com>
2022-10-11 14:31:03 +0200

    Added cpp-file to record and stream video output

---
+++ CMakeFiles/pub_video.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pub_video.dir/pub_video.cpp.o"
+  "pub_video"
+  "pub_video.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pub_video.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pub_video.cpp
@@ -0,0 +1,40 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(){
+
+  VideoCapture cap(0); 
+
+  // Check if camera opened successfully
+  if(!cap.isOpened()){
+   	cout << "Error opening video stream" << endl;
+        return -1;
+  }
+  
+  while(1) {
+
+    Mat frame;
+   
+    // Capture frame-by-frame
+    cap >> frame;
+ 
+    // If the frame is empty, break immediately
+    if (frame.empty())
+      break;
+   
+    // Display the resulting frame    
+    imshow( "Frame", frame );
+ 
+    // Press  ESC on keyboard to  exit
+    char c = (char)waitKey(1);
+    if( c == 27 ) 
+      break;
+  }
+
+  // Closes all the frames
+  destroyAllWindows();
+  return 0;
+}
