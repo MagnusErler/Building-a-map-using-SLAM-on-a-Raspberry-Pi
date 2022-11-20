e4f3aee922071ea608c9a3fa8967944287fba26f
Magnus Erler <magnus.erler@gmail.com>
2022-11-20 21:26:43 +0100

    clean-up

--- catkin_ws/src/camera/src/publisher.py
+++ catkin_ws/src/camera/src/publisher.py
@@ -5,32 +5,39 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 
-cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
-cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 500)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
-cap.set(cv2.CAP_PROP_FPS,20)
+if __name__ == '__main__':
 
-rospy.init_node('node_image', anonymous=True)
-pub = rospy.Publisher('/camera/image_raw', Image, queue_size=10)
+    # ROS
+    rospy.init_node('node_image', anonymous=True)
+    pub = rospy.Publisher('/camera/image_raw', Image, queue_size=10)
 
-# Used to convert between ROS and OpenCV images
-br = CvBridge()
+    rate = rospy.Rate(10)
 
-rate = rospy.Rate(10)
+    # CAMERA
+    cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 500)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
+    cap.set(cv2.CAP_PROP_FPS,20)
 
-while(cap.isOpened()):
+    # Used to convert between ROS and OpenCV images
+    br = CvBridge()
+    
+    while not rospy.is_shutdown():
 
-    ret, frame = cap.read()
+        if (cap.isOpened()):
 
-    # if video finished or no Video Input
-    if not ret:
-        break
+            ret, frame = cap.read()
 
-    # The 'cv2_to_imgmsg' method converts an OpenCV image to a ROS image message
-    pub.publish(br.cv2_to_imgmsg(frame, "rgb8"))
+            # if video finished or no Video Input
+            if not ret:
+                break
 
-    rospy.loginfo('publishing video frame')
+            # The 'cv2_to_imgmsg' method converts an OpenCV image to a ROS image message
+            pub.publish(br.cv2_to_imgmsg(frame, "rgb8"))
 
-    rate.sleep()  
+            rospy.loginfo('publishing video frame')
+
+            
+        rate.sleep()  
 
