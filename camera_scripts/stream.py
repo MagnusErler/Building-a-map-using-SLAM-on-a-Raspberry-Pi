60c5ad692336848970d61ca89cad375845bdc1b0
Magnus Erler <magnus.erler@gmail.com>
2022-12-18 13:24:56 +0100

    clean-up

--- camera_scripts/stream.py
+++ camera_scripts/stream.py
@@ -3,11 +3,14 @@ import time
 
 input_video_path = '/home/ubuntu/RoboCup2023/Building-a-map-using-SLAM-on-a-Raspberry-Pi/camera_scripts/recordedVideo.avi'
 
+width = 3280/10
+height = 2464/10
+
 cap = cv2.VideoCapture(input_video_path)
 cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 500)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 cap.set(cv2.CAP_PROP_FPS,20)
 
 cv2.namedWindow('frame', flags=cv2.WINDOW_GUI_NORMAL)
@@ -18,7 +21,6 @@ prev_frame_time = 0
 new_frame_time = 0
 
 def getFPS():
-    return cap.get(CAP_PROP_FPS)
     # time when we finish processing for this frame
     global prev_frame_time, new_frame_time
     new_frame_time = time.time()
@@ -40,6 +42,7 @@ while(cap.isOpened()):
 
     # putting the FPS count on the frame
     #cv2.putText(frame, str(cap.get(cv2.CAP_PROP_FPS)), (1, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (100, 255, 0), 1, cv2.LINE_AA)
+    cv2.putText(frame, str(getFPS()), (1, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 50, 0), 1, cv2.LINE_AA)
 
     cv2.imshow('frame', frame)
 
